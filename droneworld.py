bab3330b69da91e7a8b6469408d0bc31f16d2b00
k-san-official <konstantin.sandfort@gmail.com>
2023-04-23 18:52:58 +0200

    added drone sphere + controls

--- droneworld.py
+++ droneworld.py
@@ -1,22 +1,40 @@
-
+import numpy as np
 
 class DroneWorld:
     def __init__(self, size, n_people, n_obst, discount):
 
+        # World configuration
+        self.forward_step = 0.2
+
         # Define actions (forward, left, right)
         self.actions = ((1, 0), (1, -1), (1, 1))
+        self.size = size
 
         # Place people into the world (randomly)
+        self.people = []
+        for i in range(n_people):
+            self.people.append(self.get_random_location())
 
         # Place obstacles into the world (randomly)
 
         # Set random starting position
-        self.starting_pos = (0, 0)  #TODO: Implement
+        self.starting_pos = self.get_random_location()
+        self.current_pos = self.starting_pos
 
         # Set state (as a list of features)
 
+    def get_random_location(self):
+        x_r = np.random.rand() * self.size
+        y_r = np.random.rand() * self.size
+        return (x_r, y_r)
+
     def update_drone_location(self, action): # Updates the drone location for a certain action
-        pass
+        if action == 'w':
+            self.current_pos = (self.current_pos[0] + self.forward_step, self.current_pos[1])
+        elif action == 'a':
+            pass
+        elif action == 'd':
+            pass
 
     def get_state(self):  # Gets the features out of the simulation
         pass
--- simulation2d.py
+++ simulation2d.py
@@ -2,16 +2,52 @@ import tkinter as tk
 from droneworld import DroneWorld
 
 
+def get_circle_coordinates(x, y, r):
+    p1 = [x-r, y-r]
+    p2 = [x+r, y+r]
+    return [p1, p2]
+
 if __name__ == '__main__':
     # Edit these variables to change the generation of the world:
     size = 50
     n_people = 5
     n_obstacles = 5
     discount = 0.9
+    drone_angle = 0  # Rotation in the world
 
     dw = DroneWorld(size, n_people, n_obstacles, discount)
+
+    # Set up GUI
     root = tk.Tk()
     root.geometry('{}x{}'.format(size * 10 + 100, size * 10 + 100))
     canvas = tk.Canvas(root, bg='green', height=size*10, width=size*10)
     canvas.pack()
+
+    p = dw.starting_pos
+    dr = 10  # Drone radius
+    drone_sphere = canvas.create_oval(
+        (p[0] * 10) - dr,
+        (p[1] * 10) - dr,
+        (p[0] * 10) + dr,
+        (p[1] * 10) + dr,
+        fill='red')
+
+    def keypress(event):
+        """
+        Moves the drone (red circle) on the field when a button is pressed.
+        :param event:
+        :return:
+        """
+        x = 0
+        y = 0
+        dw.update_drone_location(event.char)
+        new_pos = dw.current_pos
+        canvas.moveto(
+            drone_sphere,
+            (new_pos[0] * 10) - dr,
+            (new_pos[1] * 10) - dr,)
+
+
+    root.bind("<Key>", keypress)
+
     root.mainloop()
