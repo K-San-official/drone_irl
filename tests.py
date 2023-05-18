b58d4e1402ae5d6ff5151b4971e34e08335278a3
K-San-official <63699796+K-San-official@users.noreply.github.com>
2023-05-18 15:10:24 +0200

    added unit tests

--- tests.py
+++ tests.py
@@ -1,11 +1,26 @@
 import utils as ut
+import irl
+import unittest
+
+class Testing(unittest.TestCase):
+    def testIntersect(self):
+        a = (0, 0)
+        b = (1, 1)
+        c = (0, 1)
+        d = (1, 0)
+        self.assertEqual(ut.intersect(a, b, c, d), True)
+
+    def testIntersectCoord(self):
+        a = (0, 0)
+        b = (1, 1)
+        c = (0, 1)
+        d = (1, 0)
+        self.assertEqual(ut.line_intersection_coordinates((a, b), (c, d)), (0.5, 0.5))
+
+    def testScoreCalculation(self):
+        pass
 
 if __name__ == '__main__':
-    a = (134.30229109146936, 434.916540491883)
-    b = (284.3022910914693, 434.916540491883)
-    c = (14.113893599659095, 422.32066425227714)
-    d = (4.0599547393179165, 501.3347123594574)
-    print(ut.intersect(a, b, c, d))
-    print(ut.line_intersection_coordinates((a, b), (c, d)))
+    unittest.main()
 
 
