d06f60a10fc7c52a06217cfa28bda4e83e09c7a8
K-San-official <63699796+K-San-official@users.noreply.github.com>
2023-06-16 14:59:52 +0200

    added error range (std) to barplot_generator.py

--- barplot_generator.py
+++ barplot_generator.py
@@ -3,6 +3,39 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
+    # Import values from file
+    filepath = 'reward_weights/weights_avoid_o.txt'
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    values = []
+
+    for line in lines:
+        weights_str = line.split()
+        # Convert to float
+        weights = [float(x) for x in weights_str]
+        values.append(weights)
+
+    arr = np.array(values)
+    means = arr.mean(axis=0)  # Column means
+    stds = arr.std(axis=0)  # Standard Deviations
+
+    x_values = np.arange(len(values[0]))
+
+    # Build plot
+    fig, ax = plt.subplots()
+    ax.bar(x_values, means, yerr=stds, align='center', alpha=0.5, ecolor='black', capsize=10)
+    ax.set_xlabel('Reward Weight Index')
+    ax.set_ylabel('Reward Weight Value')
+    ax.set_xticks(x_values)
+    ax.yaxis.grid(True)
+
+    # Save the figure and show
+    plt.tight_layout()
+    plt.savefig('bar_plot_with_error_bars.png')
+    plt.show()
+
+    """
+    
     values = [
         -0.0781,
         0.0057,
@@ -29,4 +62,5 @@ if __name__ == '__main__':
     plt.ylabel('Weight Value')
     plt.xticks(x)
     plt.show()
+    """
 
