95fe31eab8a92a9b5e3a61e3f641a99a84728cd8
K-San-official <63699796+K-San-official@users.noreply.github.com>
2023-06-20 15:38:41 +0200

    new results

--- ex_3.py
+++ ex_3.py
@@ -3,12 +3,12 @@ import utils
 from droneworld import DroneWorld
 
 pol_comb = [
-    #['avoid_o', 'random'],
+    ['avoid_o', 'random'],
     #['avoid_p', 'random'],
-    ['avoid_a', 'random'],
-    #['random', 'avoid_o'],
+    #['avoid_a', 'random'],
+    ['random', 'avoid_o']#,
     #['random', 'avoid_p'],
-    ['random', 'avoid_a']#,
+    #['random', 'avoid_a']#,
     #['avoid_o', 'avoid_p'],
     #['avoid_o', 'avoid_a'],
     #['avoid_p', 'avoid_o'],
